92015d9c888944d9bc3e96b27fe506405b273d0d
Santiago <santiago.sgm12@gmail.com>
2025-02-17 13:47:14 +0100

    archivo docker creado

--- microproceso1/venv1/Scripts/activate.bat
+++ microproceso1/venv1/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\gerar\OneDrive\Escritorio\DAM\ACDAT\TareaMicroProcesos\microproceso1\venv1"
+set "VIRTUAL_ENV=D:\Instituto\Segundo\PSP\Microprocesos\TareaMicroProcesos\microproceso1\venv1"
 
 if not defined PROMPT set PROMPT=$P$G
 
